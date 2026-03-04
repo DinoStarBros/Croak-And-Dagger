2c3a3a8d2a4334c459eff804dca33dd73fd99a3b
DinoStarBros <lmatthewalven@gmail.com>
2026-03-04 21:33:54 +0800

    Made title screen, installed scene manager and aseprite wizard addon

--- screens/title_screen/title_screen.gd
+++ screens/title_screen/title_screen.gd
@@ -1,2 +1,16 @@
 extends Control
 class_name TitleScreen
+
+func _ready() -> void:
+	%play.pressed.connect(_play_pressed)
+	%settings.pressed.connect(_settings_pressed)
+	%quit.pressed.connect(_quit_pressed)
+
+func _play_pressed() -> void:
+	pass
+
+func _settings_pressed() -> void:
+	pass
+
+func _quit_pressed() -> void:
+	get_tree().quit()
