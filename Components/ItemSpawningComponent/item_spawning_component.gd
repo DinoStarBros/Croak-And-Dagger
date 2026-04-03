0a7bf034ed1026fcef1a6ab52b2150ae95303a6a
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-04-03 09:31:54 +0800

    completed luck stat

--- Components/ItemSpawningComponent/item_spawning_component.gd
+++ Components/ItemSpawningComponent/item_spawning_component.gd
@@ -6,17 +6,24 @@ class_name ItemSpawningComponent
 
 @export var item_scn : PackedScene
 
-var item_chance : float 
+var item_drop_chance : float 
 
-const base_item_chance : float = 1.0/6.0
+const base_item_chance : float = 10.0/60.0
 
 func on_slash() -> void:
-	item_chance = base_item_chance / (3/Global.luck)
+	#item_drop_chance = base_item_chance * diminishing_returns_scaling(Global.luck, 5)
+	item_drop_chance = base_item_chance + linear_scaling(base_item_chance, Global.luck)
 	
 	var rand : float = randf()
 	
-	if rand < item_chance:
+	if rand < item_drop_chance:
 		_spawn_item()
 
 func _spawn_item() -> void:
 	Global.inventory.add_item(item_scn)
+
+func diminishing_returns_scaling(value: float, k: float) -> float:
+	return 1 + (value / (value+k))
+
+func linear_scaling(base_chance: float, luck:float) -> float:
+	return (1 - base_chance) * (luck / 100)
