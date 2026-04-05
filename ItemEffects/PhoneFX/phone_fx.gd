7287ab68384ce3d6b0b5a7a596d3aa0c600303ad
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-04-05 20:19:06 +0800

    uhh

--- ItemEffects/PhoneFX/phone_fx.gd
+++ ItemEffects/PhoneFX/phone_fx.gd
@@ -7,7 +7,6 @@ const item_scns : Array[PackedScene] = [
 	preload("res://Items/Energy/energy.tscn"),
 	preload("res://Items/KroFlex/kroflex.tscn"),
 	preload("res://Items/Perfume/perfume.tscn"),
-	preload("res://Items/Phone/phone.tscn"),
 	preload("res://Items/TCG/tcg.tscn"),
 	preload("res://Items/Zarchivas/zarchivas.tscn"),
 ]
