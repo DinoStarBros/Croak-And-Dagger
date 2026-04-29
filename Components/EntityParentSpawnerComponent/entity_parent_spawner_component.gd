38dbb1cd2e6ac30ab4d5ebc78ba1e8860f8b327b
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-04-29 17:40:19 +0800

    Update entity_parent_spawner_component.gd

--- Components/EntityParentSpawnerComponent/entity_parent_spawner_component.gd
+++ Components/EntityParentSpawnerComponent/entity_parent_spawner_component.gd
@@ -38,8 +38,9 @@ func _upgrade_done() -> void:
 	
 	Global.current_game_state = Global.game_states.TRANSITION_NEXT_COMBAT
 	
-	await get_tree().create_timer(0).timeout
+	#await get_tree().create_timer(0).timeout
 	#await get_tree().create_timer(Global.WAIT_TIME + randf_range(-0.2, 0.5)).timeout
+	await get_tree().create_timer(Global.WAIT_TIME).timeout
 	
 	Global.current_game_state = Global.game_states.FIGHT
 	GlobalSignals.CombatStart.emit()
